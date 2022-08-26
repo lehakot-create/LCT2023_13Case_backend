4a5ae58435e7ae7641277b8b9aa0c66681066c1e
rogiGoodwill <dolgow313@gmail.com>
2022-08-26 11:48:11 +0400

    Переопределил модель AbstractUser

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,13 @@
+from django.contrib import auth
 from django.contrib.auth.hashers import identify_hasher, make_password
+from django.contrib.auth.models import PermissionsMixin
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
+from django.core.mail import send_mail
+from django.apps import apps
 from django.utils.translation import gettext_lazy as _
 
 
@@ -86,6 +92,7 @@ class Direction(models.Model):
         verbose_name = 'направление проекта'
         verbose_name_plural = 'направления проектов'
 
+
 # Тариф
 class Rate(models.Model):
     RATE_TYPES = (
@@ -104,6 +111,7 @@ class Rate(models.Model):
         verbose_name = 'тариф'
         verbose_name_plural = 'тарифы'
 
+
 # Стадии проекта
 class Stage(models.Model):
     stage = models.JSONField()
@@ -114,6 +122,7 @@ class Stage(models.Model):
     class Meta:
         verbose_name = 'стадия проекта'
         verbose_name_plural = 'стадии проектов'
+
 
 # Таблица проектов
 class Project(models.Model):
@@ -151,12 +160,73 @@ class Project(models.Model):
         verbose_name = 'проект'
         verbose_name_plural = 'проекты'
 
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Необходимо ввести email адрес')
+        if not password:
+            raise ValueError('Необходимо ввести пароль')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+    def with_perm(self, perm, is_active=True, include_superusers=True, backend=None, obj=None):
+        if backend is None:
+            backends = auth._get_backends(return_tuples=True)
+            if len(backends) == 1:
+                backend, _ = backends[0]
+            else:
+                raise ValueError(
+                    'You have multiple authentication backends configured and '
+                    'therefore must provide the `backend` argument.'
+                )
+        elif not isinstance(backend, str):
+            raise TypeError(
+                'backend must be a dotted import path string (got %r).'
+                % backend
+            )
+        else:
+            backend = auth.load_backend(backend)
+        if hasattr(backend, 'with_perm'):
+            return backend.with_perm(
+                perm,
+                is_active=is_active,
+                include_superusers=include_superusers,
+                obj=obj,
+            )
+        return self.none()
+
+
 # Таблица пользователей
-class Profile(AbstractUser):
-
-
-
-
+class Profile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=255)
+    nick_name = models.CharField(max_length=255, default="User")
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     id_status = models.ForeignKey(Status, default=1, on_delete=models.DO_NOTHING,
                                   verbose_name='ID статуса')  # применить
@@ -166,6 +236,27 @@ class Profile(AbstractUser):
     # rating = models.ForeignKey(Project, on_delete=models.DO_NOTHING)  # Рейтинг
     telephone = models.CharField(max_length=50, verbose_name='Телефон')  # тип номер телефона
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
 
     """
     процедура создания списка стеков, т.к. при соедниении manytomany, мы получаем список записей
@@ -175,18 +266,7 @@ class Profile(AbstractUser):
         return ' '.join([str(s) for s in self.stack.all()])
 
     def __str__(self):
-        return self.username
-
-    """
-    процедура проверки пароля на хешированность перед сохранением
-    """
-
-    def save(self, *args, **kwargs):
-        try:
-            _alg = identify_hasher(self.password)
-        except ValueError:
-            self.password = make_password(self.password)
-        super().save(*args, **kwargs)
+        return self.email
 
     class Meta:
         verbose_name = 'профиль'
