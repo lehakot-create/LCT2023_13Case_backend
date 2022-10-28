7ba201fc04e831dc1f0adb4f1843dd3b4fb0718e
lehakot-create <lehakot85@inbox.ru>
2022-10-28 22:33:22 +0400

    добавлены команды миграции и заполнения БД начальными данными

--- Projects_factory/settings.py
+++ Projects_factory/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost:8000']
 
 # Application definition
 
