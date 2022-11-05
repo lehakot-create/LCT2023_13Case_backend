95a662695152781d2165bc99bde225089434142d
lehakot-create <lehakot85@inbox.ru>
2022-11-05 22:04:28 +0400

    добавил в хосты 178.170.197.171

--- Projects_factory/settings.py
+++ Projects_factory/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '178.170.197.171']
 
 # Application definition
 
